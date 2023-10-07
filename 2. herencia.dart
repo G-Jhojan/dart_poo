3ad65d47fdc2aa26e3d67a79118e58610dfa3651
Jhojan <jhojanrock98@gmail.com>
2023-10-07 12:43:06 -0500

    Herencia aplicada

---
+++ 2. herencia.dart
@@ -0,0 +1,35 @@
+class Vehiculo {
+  String marca;
+  String modelo;
+  String design;
+  int year;
+
+  Vehiculo(
+    this.marca,
+    this.modelo,
+    this.design,
+    this.year,
+  );
+
+  void mostrarDetalles() {
+    print('Marca:$marca');
+    print('Modelo:$modelo');
+    print('Diseño:$design');
+    print('Año: $year ');
+  }
+}
+
+//clase en donde se hereda de la clase vehiculo
+
+class Automovil extends Vehiculo {
+  String tipoDeCarroceria;
+
+  Automovil(String marca, String modelo, String design, int year,
+      this.tipoDeCarroceria)
+      : super(marca, modelo, design, year);
+
+  void mostrarDetallesAutomovil() {
+    super.mostrarDetalles();
+    print('Tipo de carroceria: $tipoDeCarroceria');
+  }
+}
--- main.dart
+++ main.dart
@@ -1,21 +1,29 @@
-import 'clase_basica.dart';
+//import '1. clases_y_objetos.dart';
+
+//void main() {
+//  Persona persona1 = Persona('Juan', 'Estrada', 'Ibarra', 25);
+//  Persona persona2 = Persona('María', 'Aguinaga', 'Otavalo', 25);
+//  Persona persona3 = Persona('Jose', 'María', 'Bucaramanga', 65);
+//  Persona persona4 = Persona('Mario', 'Birra', 'Argentina', 35);
+//
+//  persona1.mostrarDetalles();
+//  print('-------------------');
+//
+//  persona2.mostrarDetalles();
+//  print('-------------------');
+//
+//  persona3.mostrarDetalles();
+//  print('-------------------');
+//
+//  persona4.mostrarDetalles();
+//  print('-------------------');
+//  print('Eres la mera verga perro!');
+//}
+//
+import '2. herencia.dart';
 
 void main() {
-  Persona persona1 = Persona('Juan', 'Estrada', 'Ibarra', 25);
-  Persona persona2 = Persona('María', 'Aguinaga', 'Otavalo', 25);
-  Persona persona3 = Persona('Jose', 'María', 'Bucaramanga', 65);
-  Persona persona4 = Persona('Mario', 'Birra', 'Argentina', 35);
-
-  persona1.mostrarDetalles();
-  print('-------------------');
-
-  persona2.mostrarDetalles();
-  print('-------------------');
-
-  persona3.mostrarDetalles();
-  print('-------------------');
-
-  persona4.mostrarDetalles();
-  print('-------------------');
-  print('Eres la mera verga perro!');
+  Automovil automovil1 =
+      Automovil('Toyota', 'Hilux', '4x4', 2023, 'La Carita de Dios');
+  automovil1.mostrarDetallesAutomovil();
 }
